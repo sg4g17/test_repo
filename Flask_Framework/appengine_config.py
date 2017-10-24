f188edf428023b6264b75e23abea50b433f545a7
promer94 <yixuanxu94@gmail.com>
2017-10-24 21:07:24 +0100

    Simple Flask Framework

--- Flask_Framework/appengine_config.py
+++ Flask_Framework/appengine_config.py
@@ -12,23 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START app]
-import logging
+# [START vendor]
+from google.appengine.ext import vendor
 
-from flask import Flask
-
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return 'Hello World!'
-
-
-@app.errorhandler(500)
-def server_error(e):
-    # Log the error and stacktrace.
-    logging.exception('An error occurred during a request.')
-    return 'An internal error occurred.', 500
-# [END app]
+# Add any libraries installed in the "lib" folder.
+vendor.add('lib')
+# [END vendor]
---
+++ Flask_Framework/main.py
@@ -0,0 +1,51 @@
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START app]
+import logging
+
+# [START imports]
+from flask import Flask, render_template, request
+# [END imports]
+
+# [START create_app]
+app = Flask(__name__)
+# [END create_app]
+
+# []
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+# []
+
+# [START login]
+@app.route('/login')
+def login():
+    return render_template('login.html')
+# [END login]
+
+# [A welcome page after login]
+@app.route('/welcome')
+def login():
+    return render_template('welcome.html')
+
+
+
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
+# [END app]
